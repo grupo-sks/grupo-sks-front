ad8a98c571e54f6d3a32d434e121cbe244f130d0
aenewsss <aenamartinelli@gmail.com>
2023-06-16 18:37:09 -0300

    ok

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+const incidenteForm = document.getElementById('incidente-form');
+const dialog = document.getElementById('dialog');
+
+incidenteForm.addEventListener('submit', () => {
+    dialog.showModal()
+})
