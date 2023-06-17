d91c560fa393e7f2dc978e842dd4caafade7a093
aenewsss <aenamartinelli@gmail.com>
2023-06-17 15:10:56 -0300

    ok

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,12 @@
 const incidenteForm = document.getElementById('incidente-form');
+const modalForm = document.getElementById('modal-form');
 const dialog = document.getElementById('dialog');
 
 incidenteForm.addEventListener('submit', () => {
     dialog.showModal()
+})
+
+modalForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    window.location.href = "status-do-incidente.html"
 })
